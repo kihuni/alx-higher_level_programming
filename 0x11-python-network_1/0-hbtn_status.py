550615d4c30cee47bd6a00aa7729de3a95a79e7a
stephen kihuni <stephenkihuni55@gmail.com>
2022-10-04 09:14:39 +0300

    modified task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-"""Fetch
-https://intranet.hbtn.io/status
-using urlib package
-"""
-
+""" Python script that fetches https://intranet.hbtn.io/status """
 import urllib.request
 
-if __name__ == '__main__':
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as request:
-        response = request.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(response)))
-        print("\t- content: {}".format(response))
-        print("\t- utf8 content: {}".format(response.decode('UTF-8')))
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode("utf-8")))
